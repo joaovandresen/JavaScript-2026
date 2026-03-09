c3af50fe295d96ca1a75d868011feacc657e33a8
João Victor <joao.vandresen.costa@escola.pr.gov.br>
2026-03-09 16:44:15 +0000

    exercícios passados na última aula realizados

---
+++ desafio03.js
@@ -0,0 +1,15 @@
+let nomeHeroi = "Aragorn";
+let pontosXP = 750;
+let categoria;
+
+if (pontosXP < 100) {
+    categoria = "Iniciante";
+} else if (pontosXP < 500) {
+    categoria = "Guerreiro";
+} else if (pontosXP < 1000) {
+    categoria = "Mestre";
+} else {
+    categoria = "Lenda Viva";
+}
+
+console.log("O herói " + nomeHeroi + " possui " + pontosXP + " de experiência e seu rank atual é: " + categoria);
